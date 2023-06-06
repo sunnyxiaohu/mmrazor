41e4863cc2d4db1dffd39f262dc5150c36faea9c
wangshiguang <wangshiguang@microbt.com>
2023-06-06 02:40:42 +0000

    clear codes and add ov_estimator

--- projects/face-recognition/configs/mbf_8xb256_wf42m_pfc02.py
+++ projects/face-recognition/configs/mbf_8xb256_wf42m_pfc02.py
@@ -2,6 +2,8 @@ _base_ = ['mmcls::_base_/default_runtime.py']
 
 custom_imports = dict(
     imports=[
+        'projects.commons.models.task_modules.estimators.ov_estimator',
+        'projects.commons.engine.runner.subnet_ov_val_loop',
         'projects.face-recognition.models.algorithms.spos_partialfc',
         'projects.face-recognition.models.losses.margin_loss',
         'projects.face-recognition.models.heads.partialfc_head',
@@ -9,7 +11,7 @@ custom_imports = dict(
         'projects.face-recognition.datasets.mx_face_dataset',
         'projects.face-recognition.evaluation.match_rank',
         'projects.face-recognition.engine.runner.loopx',
-        'projects.face-recognition.engine.optimizers.optimizer_constructor.py',
+        'projects.face-recognition.engine.optimizers.optimizer_constructor',
     ],
     allow_failed_imports=False)
 
@@ -28,14 +30,14 @@ architecture = dict(
     # TODO: replace model config
     backbone=dict(
         type='mmrazor.MobileFaceNet',
-        num_features=256,
+        num_features=512,
         fp16=False,
         scale=1,
         blocks=(2, 4, 6, 2)),
     # neck=dict(type='GlobalAveragePooling'),
     head=dict(
         type='mmrazor.PartialFCHead',
-        embedding_size=256,
+        embedding_size=512,
         num_classes=2059906,
         sample_rate=0.2,
         fp16=False,
@@ -66,7 +68,7 @@ train_pipeline = [
     dict(type='PackClsInputs'),
 ]
 train_dataloader = dict(
-    batch_size=256,
+    batch_size=512,
     num_workers=3,
     drop_last=True,
     pin_memory=True,
@@ -78,13 +80,81 @@ train_dataloader = dict(
 )
 
 mdataset_type = 'mmrazor.MatchFaceDataset'
-test_pipeline = [
+val_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='PackClsInputs',
         meta_keys=('sample_idx_identical_mapping', 'dataset_name')),
 ]
 val_dataloader = dict(
+    batch_size=128,
+    num_workers=1,
+    dataset=dict(
+        type='ConcatDataset',
+        datasets=[
+            dict(
+                type=mdataset_type,
+                dataset_name='120P',
+                data_root='/mnt/data/face_data/OV_test/face_recognition/120P',
+                key_file='120p.key',
+                data_prefix=dict(img_path='norm_facex'),
+                pipeline=val_pipeline),
+            dict(
+                type=mdataset_type,
+                dataset_name='baby_500p',
+                data_root=
+                '/mnt/data/face_data/OV_test/face_recognition/baby_500p',
+                key_file='baby_500p.key',
+                data_prefix=dict(img_path='norm_face'),
+                pipeline=val_pipeline),
+            dict(
+                type=mdataset_type,
+                dataset_name='glint1k',
+                data_root=
+                '/mnt/data/face_data/OV_test/face_recognition/glint1k',
+                key_file='glint1k_val.key',
+                data_prefix=dict(img_path='images'),
+                pipeline=val_pipeline),
+            dict(
+                type=mdataset_type,
+                dataset_name='mask_369p',
+                data_root=
+                '/mnt/data/face_data/OV_test/face_recognition/mask_369p',
+                key_file='mask_369p.key',
+                data_prefix=dict(img_path='norm_face'),
+                pipeline=val_pipeline),
+            dict(
+                type=mdataset_type,
+                dataset_name='menjin_20p',
+                data_root=
+                '/mnt/data/face_data/OV_test/face_recognition/menjin_20p',
+                key_file='menjin_20p.key',
+                data_prefix=dict(img_path='norm_face'),
+                pipeline=val_pipeline),
+            dict(
+                type=mdataset_type,
+                dataset_name='xm14',
+                data_root='/mnt/data/face_data/OV_test/face_recognition/xm14',
+                key_file='xm14.key',
+                data_prefix=dict(img_path='norm_112'),
+                pipeline=val_pipeline),
+        ]),
+    sampler=dict(type='DefaultSampler', shuffle=False),
+)
+
+val_evaluator = dict(type='mmrazor.Rank1')
+
+# If you want standard test, please manually configure the test dataset
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    # Since `data_preprocessor` will not be composed into onnx model when using `tensor` mode for foward,
+    # We have to move the corresonding transforms to here.
+    dict(type='Normalize', mean=127.5, std=127.5),
+    dict(
+        type='PackClsInputs',
+        meta_keys=('sample_idx_identical_mapping', 'dataset_name')),
+]
+test_dataloader = dict(
     batch_size=128,
     num_workers=1,
     dataset=dict(
@@ -140,10 +210,6 @@ val_dataloader = dict(
     sampler=dict(type='DefaultSampler', shuffle=False),
 )
 
-val_evaluator = dict(type='mmrazor.Rank1')
-
-# If you want standard test, please manually configure the test dataset
-test_dataloader = val_dataloader
 test_evaluator = val_evaluator
 
 # optimizer
@@ -192,10 +258,25 @@ param_scheduler = [
 train_cfg = dict(
     type='mmrazor.EpochBasedTrainLoopX', max_epochs=10, val_interval=1)
 val_cfg = dict()
-test_cfg = dict()
+test_cfg = dict(
+    type='mmrazor.SubnetOVValLoop',
+    evaluate_fixed_subnet=True,
+    calibrate_sample_num=0,
+    estimator_cfg=dict(
+        type='mmrazor.OVResourceEstimator',
+        ovmodel_cfg=dict(
+            work_dir='work_dirs/mbf_8xb256_wf42m_pfc02',
+            qdef_file_dir='projects/commons/ov_qdefs',
+            qfnodes='qdef_ifm_q8.qfnodes',
+            qfops='qdef_q8.qfops',
+            ifmq='q8',
+            # Uncomment and adjust `num_infer` for QoR
+            # infer_metric=test_evaluator,
+            num_infer=200,
+            num_calib=5)))
 
 # NOTE: `auto_scale_lr` is for automatically scaling LR,
 # based on the actual training batch size.
-auto_scale_lr = dict(base_batch_size=256)
+auto_scale_lr = dict(base_batch_size=512)
 
 # _base_.default_hooks.logger.interval = 10
