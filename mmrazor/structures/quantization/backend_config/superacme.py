a0cba3e3bcfe1e7cd9063ca86ba0ae578d497360
wangshiguang <wangshiguang@microbt.com>
2025-03-19 09:28:33 +0000

    fix little

--- mmrazor/structures/quantization/backend_config/superacme.py
+++ mmrazor/structures/quantization/backend_config/superacme.py
@@ -40,17 +40,15 @@ def get_superacme_backend_config() -> BackendConfig:
         output_dtype=torch.qint8,
     )
 
-    share_configs = []
+    overide_share_configs = []
     share_ops = (torch.cat, torch.nn.functional.upsample,
-                 torch.nn.Upsample,
-                 torch.nn.Identity, torch.nn.ReLU6, torch.nn.MaxPool1d,
-                 torch.nn.MaxPool2d, torch.nn.MaxPool3d)
+                 torch.nn.Upsample,)
     for op in share_ops:
         share_config = BackendPatternConfig(op) \
             .set_observation_type(
                 ObservationType.OUTPUT_SHARE_OBSERVER_WITH_INPUT) \
             .add_dtype_config(non_weighted_op_qint8_dtype_config)
-        share_configs.append(share_config)
+        overide_share_configs.append(share_config)
 
     conv_dtype_configs = [
         weighted_op_qint8_dtype_config,
@@ -75,13 +73,13 @@ def get_superacme_backend_config() -> BackendConfig:
     # during fx2trt conversion and can support them after that
     return BackendConfig('superacme') \
         .set_backend_pattern_configs(_get_conv_configs(conv_dtype_configs)) \
-        .set_backend_pattern_configs(share_configs) \
         .set_backend_pattern_configs(
             _get_linear_configs(linear_dtype_configs)) \
         .set_backend_pattern_configs(
             _get_binary_op_configs(binary_op_dtype_configs)) \
         .set_backend_pattern_configs(
             _get_share_qparams_op_configs(share_qparams_op_dtype_configs)) \
+        .set_backend_pattern_configs(overide_share_configs) \
         .set_backend_pattern_config(size_config) \
         .set_backend_pattern_configs(_get_dynamicconv_configs(conv_dtype_configs)) \
         .set_backend_pattern_configs(_get_dynamiclinear_configs(linear_dtype_configs))
